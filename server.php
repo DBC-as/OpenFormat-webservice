4d7a8bc886829fb249a03f0a5dbfcc5f56f78166
fvs <fvs@30e9dc4a-7611-4fbc-83ba-eecf79b76bd0>
2011-06-21 14:18:51 +0000

    mockup service
    git-svn-id: svn://oss.dbc.dk/sync_php/php/OpenLibrary/OpenFormat/trunk@30665 30e9dc4a-7611-4fbc-83ba-eecf79b76bd0

--- server.php
+++ server.php
@@ -47,7 +47,14 @@ class openFormat extends webServiceServer {
     */
 
     public function formatSingleManifestation($param) {
-
+        if (!$this->aaa->has_right('openformat', 500))
+            $res->error->_value = 'authentication_error';
+        else {
+            $res->bibliotekdkFullDisplay->_namespace = $this->xmlns['ofo'];
+            $res->bibliotekdkFullDisplay->_value->displayInformation->_namespace = $this->xmlns['ofo'];
+            $res->bibliotekdkFullDisplay->_value->displayInformation->_value = 
+               $this->format_rec($param->originalData->_value, $param->outputFormat->_value);
+        }
         //var_dump($res); var_dump($param); die();
         $ret->formatSingleManifestationResponse->_value = $res;
         return $ret;
@@ -65,6 +72,26 @@ class openFormat extends webServiceServer {
         echo 'aaa_credentials     ' . $this->strip_oci_pwd($this->config->get_value('aaa_credentials', 'aaa')) . '<br/>';
         echo '</pre>';
         die();
+    }
+
+    /** \brief Format the record according to format
+     *
+     */
+    private function format_rec(&$rec, $format) {
+    // fake output
+        $ret->creator->_value = '<div class="creator">Hans Engell</div>';
+        $ret->creator->_namespace = $this->xmlns['ofo'];
+        $ret->title->_value = '<div class="title">På Slotsholmen</div>';
+        $ret->title->_namespace = $this->xmlns['ofo'];
+        $ret->type->_value = '<div class="type">Bog</div></ofo:type> ';
+        $ret->type->_namespace = $this->xmlns['ofo'];
+        $ret->description->_value = '<div class="description"><div class="subjects"><span class="subjectHeader">Emne:</span><span class="subject">1945-1999</span><span class="punctuation">;</span><span class="subject">96.72</span><span class="punctuation">;</span><span class="subject">Det Konservative Folkeparti</span><span class="punctuation">;</span><span class="subject">Hans Engell</span><span class="punctuation">;</span><span class="subject">Engell, Hans</span><span class="punctuation">;</span><span class="subject">erindringer</span><span class="punctuation">;</span><span class="subject">historie</span><span class="punctuation">;</span><span class="subject">politik</span><span class="punctuation">;</span><span class="subject">politikere</span><span class="punctuation">;</span><span class="subject">politiske forhold</span><span class="punctuation">;</span><span class="subject">politiske partier</span></div><div class="abstract">Hans Engell (f. 1948), der i 1997 gik af som konservativ partileder, beskriver det politiske liv på Slotsholmen fra han i 1978 startede som pressechef til han i 1993 sluttede som justitsminister, da Tamilsagen tvang Schlüter-regeringen til at gå</div></div>';
+        $ret->description->_namespace = $this->xmlns['ofo'];
+        $ret->details->_value = '<div class="details"><div class="format">455 sider, illustreret</div><div class="formHeader">Form: </div><div class="form">erindringer</div><div class="shelfHeader">Opstilling i folkebiblioteker: <div class="shelf">96.72</div><div class="note"><div class="noteHeader">Samhørende: </div><div class="noteLink">På Slotsholmen</div> ; <div class="noteLink">Farvel til Slotsholmen</div></div><div class="isbn">ISBN: 87-11-15086-6</div><div class="price">Pris ved udgivelsen: kr. 149,00</div></div>';
+        $ret->details->_namespace = $this->xmlns['ofo'];
+        $ret->publicationDetails->_value = '<div class="publication"><div class="edition">1. udgave, 3. oplag</div>. <div class="publisher">Aschehoug</div>, <div class="year">1997</div></div>';
+        $ret->publicationDetails->_namespace = $this->xmlns['ofo'];
+        return $ret;
     }
 
     private function strip_oci_pwd($cred) {
